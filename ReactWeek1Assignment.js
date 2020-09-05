c097ce1dbde083b689216041010beb31055cf5ad
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-05 09:40:59 -0700

    second commit

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(name, email, community) {
+        this.name;
+        this.email;
+        this.community;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students) {
+        this.name;
+        this.level;
+        this.students;
+    }
+}
---
+++ ReactWeek1Playground.js
@@ -0,0 +1,26 @@
+
+//begin person object
+const person = {
+    name: 'carter',
+    age: 26,
+    log: function () {
+        console.log(this.name);
+    }
+};
+
+console.log(`${person.name} ${person.age}`);
+
+//add's "last name" section to person object
+person.lastName = 'steinhoff';
+
+person.log();
+
+const bicycle = {
+    color: 'blue',
+    electric: false,
+    start() {
+        console.log('You begin to pedal the bike');
+    }
+}
+
+bicycle.start();
