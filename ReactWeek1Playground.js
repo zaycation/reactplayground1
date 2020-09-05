c931f0eeff3387e724a90e363b0a791c82d1d36b
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-05 09:57:47 -0700

    fifth commit

--- ReactWeek1Playground.js
+++ ReactWeek1Playground.js
@@ -1,3 +1,4 @@
+//challenge 1
 //begin person object
 const person = {
   name: "carter",
@@ -12,14 +13,24 @@ console.log(`${person.name} ${person.age}`);
 //adds "last name" section to person object
 person.lastName = "John";
 
+
 person.log();
+
+
+
+//challenge 2
 
 const bicycle = {
   color: "blue",
   electric: false,
   start() {
     console.log("You begin to pedal the bike");
+    console.log("Use 'pedalFaster()' to pedal faster!")
   },
+  pedalFaster() {
+      console.log("You start pedalling faster!")
+  }
 };
 
 bicycle.start();
+bicycle.pedalFaster();
