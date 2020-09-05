efb8d31dd4bbb42d26617ddd336e25c65aa505b1
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-05 12:33:30 -0700

    thirteenth commit

--- week-1/ReactWeek1Assignment.js
+++ week-1/ReactWeek1Assignment.js
@@ -1,15 +1,36 @@
 class Student {
-    constructor(name, email, community) {
-        this.name;
-        this.email;
-        this.community;
-    }
+  constructor(name, email, community) {
+    this.name = name;
+    this.email = email;
+    this.community = community;
+  }
 }
 
 class Bootcamp {
-    constructor(name, level, students=[]) {
-        this.name;
-        this.level;
-        this.students;
+  constructor(name, level, students = []) {
+    this.name = name;
+    this.level = level;
+    this.students = students;
+  }
+  registerStudent(student) {
+    const dupeEmail = this.students.filter(
+      (item) => item.email === student.email
+    );
+    if (dupeEmail.length === 0) {
+      this.students.push(student);
+      console.log("A student has now been added");
+      return;
+    } else {
+      console.log("Student you are trying to register already exists");
     }
+  }
 }
+
+const student1 = new Student("Isaiah", "isaiahthomas098@gmail.com", "ReactWk1");
+const student2 = new Student("Tim", "isaiahthomas098@gmail.com", "ReactWk1");
+const bootcamp1 = new Bootcamp("Isaiah", "Advanced");
+
+bootcamp1.registerStudent(student1);
+bootcamp1.registerStudent(student2);
+
+console.log(bootcamp1.students);
